f37d1f68843d3f0e5c286f8476475483a07d6b96
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-24 16:11:42 -0500

    added routes to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import VendingMachine from './VendingMachine';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Doritos from "./Doritos"
+import Pancakes from "./Pancakes"
+import Snickers from "./Snickers"
 
 function App() {
   return (
     <div className="App">
-      <VendingMachine />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<VendingMachine />} />
+          <Route path="/food/doritos" element={<Doritos />} />
+          <Route path="/food/pancakes" element={<Pancakes />} />
+          <Route path="/food/snickers" element={<Snickers />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
