ff00a90e3d1cb6f438bc452142f2d0fdeb641ea2
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-24 16:17:05 -0500

    added items to the vending machine

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { BrowserRouter, useNavigate, Route, Routes } from "react-router-dom";
-import Food from "./Food"
+import { Link } from "react-router-dom";
 
 function VendingMachine() {
   return (
     <div className="VendingMachine">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/food/:name" element={<Food />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Welcome to our vending machine!</h1>
+      <h4>Select an item below!</h4>
+      <ul>
+        <li>
+          <Link to="/food/doritos">Doritos</Link>
+        </li>
+        <li>
+          <Link to="/food/pancakes">Pancakes</Link>
+        </li>
+        <li>
+          <Link to="/food/snickers">Snickers</Link>
+        </li>
+      </ul>
     </div>
   );
 }
